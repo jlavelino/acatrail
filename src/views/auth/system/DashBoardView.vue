475fe9c6f5e7e8ae05599e11bdb68b25278251f0
JhonLeovil <jhonleovil@.com>
2024-12-04 11:11:12 +0800

    feat: added profile header and dashboard view

---
+++ src/views/auth/system/DashBoardView.vue
@@ -0,0 +1,18 @@
+<script setup>
+import AppLayout from '@/components/layout/AppLayout.vue'
+
+import { ref } from 'vue'
+
+const isDrawerVisible = ref(true)
+</script>
+
+<template>
+  <AppLayout
+    :is-with-app-bar-nav-icon="true"
+    @is-drawer-visible="isDrawerVisible = !isDrawerVisible"
+  >
+    <template #content>
+      <v-container></v-container>
+    </template>
+  </AppLayout>
+</template>
