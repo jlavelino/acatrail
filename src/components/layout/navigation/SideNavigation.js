9ced64761338833288bd37e1d2cc5a969b2c7424
jlavelino <jhonleovil@gmail.com>
2024-12-23 12:08:51 +0800

    feat: added side navigation

---
+++ src/components/layout/navigation/SideNavigation.js
@@ -0,0 +1,13 @@
+// 👉 Main Navigation; Title, Icon
+export const mainNav = [
+  ['User Management', 'mdi-account-badge-outline'],
+  ['Form', 'mdi-form-select'],
+  ['Benefits', 'mdi-list-box-outline'],
+  ['About us', 'mdi-account-badge-outline'],
+]
+
+// 👉 Sub Navigations; Title, Icon, Subtitle, Redirect Path
+export const menuItemsNav1 = [['User Roles', 'mdi-tag-multiple', '', '/manage/user/roles']]
+export const menuItemsNav2 = [['Form', 'mdi-form-textarea']]
+export const menuItemsNav3 = [['Benefits', 'mdi-tray-arrow-down']]
+export const menuItemsNav4 = [['About', 'mdi-account-box-outline']]
