0e9d885569772b9c3786dcf5004302d3133d356b
jlavelino <jhonleovil@gmail.com>
2024-12-25 11:11:00 +0800

    fix: removing and renaming the list in side naviagtion

--- src/components/layout/navigation/SideNavigation.js
+++ src/components/layout/navigation/SideNavigation.js
@@ -1,13 +1,11 @@
 // 👉 Main Navigation; Title, Icon
 export const mainNav = [
-  ['User Management', 'mdi-account-badge-outline'],
-  ['Form', 'mdi-form-select'],
-  ['Benefits', 'mdi-list-box-outline'],
-  ['About us', 'mdi-account-badge-outline'],
+  ['Your Tasks', ' mdi-notebook-multiple'],
+  ['List of Subjects', ' mdi-bookshelf  '],
 ]
 
 // 👉 Sub Navigations; Title, Icon, Subtitle, Redirect Path
-export const menuItemsNav1 = [['User Roles', 'mdi-tag-multiple', '', '/manage/user/roles']]
-export const menuItemsNav2 = [['Form', 'mdi-form-textarea']]
-export const menuItemsNav3 = [['Benefits', 'mdi-tray-arrow-down']]
-export const menuItemsNav4 = [['About', 'mdi-account-box-outline']]
+export const menuItemsNav1 = [
+  ['Assignments', 'mdi-pencil-box-outline'],
+  ['Projects', 'mdi-book-multiple'],
+]
